fa7797318d4a2eff22a04f15a22b3af73c08cc96
Nicolas <nickrios33@yahoo.com>
2020-05-21 19:00:47 -0700

    added models and added properties

--- Tips/models/LSITip.h
+++ Tips/models/LSITip.h
@@ -12,6 +12,11 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface LSITip : NSObject
 
+@property(nonatomic,readonly,copy)NSString*name;
+@property(nonatomic,readonly)double total;
+@property(nonatomic,readonly)NSInteger splitCount;
+@property(nonatomic,readonly)double tipCount;
+
 @end
 
 NS_ASSUME_NONNULL_END
